928423c42f8f22e9dc50a3a6ff9755da1bc2648b
YaseenBabaShaik <babashaik.y@gmail.com>
2024-07-10 21:50:00 +0530

    k

--- loops/count/main.tf
+++ loops/count/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "web" {
-  count         = 2 
+  count         = 1
   ami           = data.aws_ami.centos8.id
   instance_type = "t3.micro"
 
@@ -16,5 +16,5 @@ data "aws_ami" "centos8" {
 }
 
 output "publicip" {
-    value = aws_instance.web.public_ip
+    value = aws_instance.web.*.public_ip
 }
